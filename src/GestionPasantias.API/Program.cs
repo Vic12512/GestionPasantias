f39c79948b4a91d4218dc539eeeea0b5754db733
Vic12512 <victoreliecer165@gmail.com>
2026-03-05 16:47:03 -0400

    fix(API): enable swagger outside development enviroment

--- src/GestionPasantias.API/Program.cs
+++ src/GestionPasantias.API/Program.cs
@@ -5,10 +5,11 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 // register Controllers
 builder.Services.AddControllers();
-
-builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -18,10 +19,15 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+/*
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+*/
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
@@ -29,4 +35,3 @@ app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
-

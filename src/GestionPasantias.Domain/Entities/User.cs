1bb01ed11131234246a2f5017da4832ac78f0fe0
Vic12512 <victoreliecer165@gmail.com>
2026-03-04 23:30:35 -0400

    feat(domain): add User entity

---
+++ src/GestionPasantias.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+namespace GestionPasantias.Domain.Entities;
+
+public class usuario
+{
+    public Guid Id {get; set; }
+    public string Name {get; set; } = null!;
+    public string Email {get; set; } = null!;
+    public string PasswordHash {get; set; } = null!;
+    public string Rol {get; set; } = null!;
+}
